97ce4b462f863050a840891f11baae27443b883b
naeimzarei <naeim.zarei@gmail.com>
2017-09-26 22:38:43 -0400

    Add majority of linked list functions

--- linked-list/src/App.tsx
+++ linked-list/src/App.tsx
@@ -16,14 +16,28 @@ export default class App extends React.Component<AppProps, AppState> {
 	}
 
 	render() {
-		// this.props.store.addLast(4);
-		// this.props.store.addLast(5);
-		// this.props.store.addLast(-1);
-		// this.props.store.addLast(9);
+		this.props.store.addLast(4);
+		this.props.store.addLast(5);
+		this.props.store.addLast(-1);
+		this.props.store.addLast(9);
 		this.props.store.addIndex(0, 0);
 		this.props.store.addIndex(0, 43);
 		this.props.store.addIndex(0, 84);
 		this.props.store.addIndex(1, 44);
+		this.props.store.removeFirst();
+		this.props.store.removeLast();
+		this.props.store.removeLast();
+		this.props.store.removeLast();
+		this.props.store.removeLast();
+		this.props.store.addLast(10);
+		this.props.store.addLast(12);
+		this.props.store.addLast(14);
+		this.props.store.addLast(15);
+		this.props.store.addLast(19);
+		this.props.store.removeIndex(4);
+		this.props.store.removeIndex(0);
+		this.props.store.removeIndex(2);
+		this.props.store.addFirst(39);
 		return (
 			<div>
 				{this.props.store.root}
--- linked-list/src/Store.tsx
+++ linked-list/src/Store.tsx
@@ -7,9 +7,13 @@ export default class Store extends React.Component {
 	@observable root: any = null;
 	// the number of nodes in linked list
 	@observable count: number = 0;
-	// set the number of nodes in linked list
+	// increment count by 1
 	@action.bound increment(): void {
 		this.count++;
+	}
+	// decrement count by 1
+	@action.bound decrement(): void {
+		this.count--;
 	}
 	// set the root linked list node
 	@action.bound setRoot(root: any): void {
@@ -27,9 +31,32 @@ export default class Store extends React.Component {
 	 * Obtain node at given index
 	 * @param {number} index
 	 */
-	//  getNode(index: number): Node {
-
+	//  getNodeIndex(index: number): Node {
 	//  }
+
+	/**
+	 * Obtain node given value
+	 * @param {number} value
+	 */
+	// getNodeValue(value: number) {
+
+	// }
+
+	/**
+	 * Add a node at the root level
+	 * @param {number} value 
+	 */
+	addFirst(value: number): void {
+		// there are no nodes
+		if (this.count === 0) {
+			this.setRoot(<Node value={value} next={null}/>);
+		} else if (this.count === 1) {
+			this.setRoot(<Node value={value} next={null}/>);
+		} else {
+			let newNode = <Node value={value} next={this.root}/>;
+			this.setRoot(newNode);
+		}
+	}
 
 	/**
 	 * Adds node to end of linked list
@@ -37,13 +64,16 @@ export default class Store extends React.Component {
 	 */
 	addLast(value: number): void {
 		if (this.root === null) {
-			this.addInitial(value); return;
+			this.addInitial(value); 
+			this.increment();
+			return;
 		}
 		let nodeCursor: any = this.root;
 		while (nodeCursor.props.next !== null) {
 			nodeCursor = nodeCursor.props.next;
 		}
 		nodeCursor.props.next = <Node value={value} next={null} />;
+		this.increment();
 	}
 
 	/**
@@ -52,11 +82,9 @@ export default class Store extends React.Component {
 	 * @param {number} value
 	 */
 	addIndex(index: number, value: number): void {
-		if (index !== 0) {
-			if (index < 0 || index >= this.count) {
-				console.log('Index cannot be less than 0 or greater than or equal to number of elements in linked list.');
-				return;
-			}
+		if (index < 0 || index > this.count) {
+			console.log('Index cannot be less than 0 or greater than or equal to number of elements in linked list.');
+			return;
 		}
 		if (this.root === null) {
 			this.addInitial(value);
@@ -64,7 +92,7 @@ export default class Store extends React.Component {
 			return;
 		}
 
-		let nodeCursor: any = this.root;
+		let nodeCursor = this.root;
 		for (let i = 0; i < this.count; i++) {
 			let currentNode = nodeCursor;
 			let nextNode = nodeCursor.props.next;
@@ -76,6 +104,89 @@ export default class Store extends React.Component {
 				nodeCursor = nodeCursor.props.next;
 			}
 		}
+	}
+
+	/**
+	 * Remove the root node
+	 */
+	removeFirst(): void {
+		// there are no nodes
+		if (this.count === 0) {
+			console.log('There are no nodes to remove from linked list.');
+			return;
+		} else if (this.count === 1) {
+			this.setRoot(null);
+			this.decrement();
+		} else {
+			this.setRoot(this.root.props.next);
+			this.decrement();
+		}
+	}
+
+	/**
+	 * Remove the last node
+	 */
+	removeLast(): void {
+		if (this.count === 0) {
+			console.log('There are no nodes to remove from linked list.');
+			return;
+		} else if (this.count === 1) {
+			this.setRoot(null);
+			this.decrement();
+		} else {
+			let nodeCursor = this.root;
+			let previousNode = null;
+			while (nodeCursor.props.next !== null) {
+				previousNode = nodeCursor;
+				nodeCursor = nodeCursor.props.next;
+			}
+			previousNode.props.next = null;
+			this.decrement();
+		}
+	}
+
+	/**
+	 * Remove node at given index.
+	 * @param {number} index
+	 */
+	removeIndex(index: number): void {
 		console.log(this.count);
+		if (index === 0) {
+			this.removeFirst();
+			return;
+		}
+
+		if (index < 0 || index > this.count) {
+			console.log('Index cannot be less than 0 or greater than number of elements in linked list.');
+			return;
+		}
+		if (this.count === 0) {
+			console.log('There are no nodes to remove from linked list.');
+			return;
+		}
+
+		if (this.count === 1) {
+			this.removeFirst();
+			return;
+		}
+
+		if (index === (this.count - 1)) {
+			console.log('Deleting last node.');
+			this.removeLast();
+			return;
+		}
+
+		let nodeCursor = this.root;
+		let previousNode;
+		for (let i = 0; i < this.count; i++) {
+			let nextNode = nodeCursor.props.next;
+			if (i === index) {
+				previousNode.props.next = nextNode;
+				this.decrement();
+			} else {
+				previousNode = nodeCursor;
+				nodeCursor = nodeCursor.props.next;
+			}
+		}
 	}
 }
